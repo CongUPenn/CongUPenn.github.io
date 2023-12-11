462efae11afe7b72fdc49df943c4061aad039d15
CongUPenn <146009737+CongUPenn@users.noreply.github.com>
2023-12-11 08:55:18 +0100

    Update sketch.js

--- s3final/sketch.js
+++ s3final/sketch.js
@@ -1,1 +1,39 @@
+let userInteractiveElement;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // Initialize your user-interactive element
+  userInteractiveElement = new UserInteractiveElement();
+}
+
+function draw() {
+  // Generate your design with conditional loops and randomness
+  background(255);
+  // Call functions to draw generative design
+  drawGenerativeDesign();
+  // Update and display user-interactive element
+  userInteractiveElement.update();
+  userInteractiveElement.display();
+}
+
+function drawGenerativeDesign() {
+  // Implement your generative design logic here
+  // Use conditional loops and randomness
+}
+
+class UserInteractiveElement {
+  constructor() {
+    // Initialize properties for user-interactive element
+  }
+
+  update() {
+    // Update properties based on user interaction
+  }
+
+  display() {
+    // Display the user-interactive element
+  }
+}
+
+// Additional p5.js functions for user interactions (e.g., mousePressed, keyPressed) can be added as needed.
 
